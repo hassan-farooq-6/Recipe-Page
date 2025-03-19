4f5af4a15de5b262b39ddc4ebe46772cca2ce63d
Hassan <hassanfarooq2190@gmail.com>
2025-03-19 19:13:38 +0500

    Test bad code

--- bad_code.py
+++ bad_code.py
@@ -1,1 +1,6 @@
-print('Hello World'
+# bad_code.py
+def divide_numbers(a, b):
+    return a / b  # Potential division by zero error
+
+result = divide_numbers(10, 0)  # This will cause a runtime error
+print(result)
