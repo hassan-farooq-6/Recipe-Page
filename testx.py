a8e4a0c8981f9828acd6e3e98b05c9edc9059541
Hassan <hassanfarooq2190@gmail.com>
2025-03-21 11:06:55 +0500

    OKH

---
+++ testx.py
@@ -0,0 +1,12 @@
+# This program prints Hello, world!
+
+print('Hello, world!')# This program prints Hello, world!
+
+print('Hello, bb!')
+# This program prints Hello, world!
+
+print('Hello, cc!')
+# This program prints Hello, world!
+
+print('Hello, aa!')
+
